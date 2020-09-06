b541fd6dd2ed1bea36cfa7674c68b92dca2804dd
1ud0v1c <ludovic.vimont@gmail.com>
2020-09-06 12:19:00 +0200

    Parse size option

--- src/ProgramArgs.kt
+++ src/ProgramArgs.kt
@@ -1,9 +1,15 @@
 class ProgramArgs(args: Array<String>) {
+    companion object {
+        const val COLOR_OPTION = "-c"
+        const val SIZE_OPTION = "-s"
+    }
+
     var color: String? = null
+    var size: Size? = null
 
     init {
-        if (args.contains("-c")) {
-            val colorIndex = args.indexOf("-c") + 1
+        if (args.contains(COLOR_OPTION)) {
+            val colorIndex = args.indexOf(COLOR_OPTION) + 1
             if (colorIndex < args.size) {
                 val element = args[colorIndex]
                 if (isColorValid(element)) {
@@ -15,6 +21,23 @@ class ProgramArgs(args: Array<String>) {
                 println("You need to seize a color.")
             }
         }
+
+        if (args.contains(SIZE_OPTION)) {
+            val sizeIndex = args.indexOf(SIZE_OPTION) + 1
+            if (sizeIndex + 1 < args.size) {
+                val stringWidth = args[sizeIndex]
+                val stringHeight = args[sizeIndex+1]
+                if (isSizeValid(stringWidth, stringHeight)) {
+                    val width = stringWidth.toInt()
+                    val height = stringHeight.toInt()
+                    size = Size(width, height)
+                } else {
+                    println("You need to seize a valid size, this one is invalid: [$stringWidth,$stringHeight].")
+                }
+            } else {
+                println("You need to seize an height and a width.")
+            }
+        }
     }
 
     private fun isColorValid(element: String): Boolean {
@@ -23,5 +46,14 @@ class ProgramArgs(args: Array<String>) {
             return results.isNotEmpty()
         }
         return false
+    }
+
+    private fun isSizeValid(width: String,
+                            height: String): Boolean {
+        return isInteger(width) && isInteger(height)
+    }
+
+    private fun isInteger(element: String): Boolean {
+        return element.toIntOrNull()?.let { true } ?: false
     }
 }
---
+++ src/Size.kt
@@ -0,0 +1,2 @@
+data class Size(val width: Int,
+                val height: Int)
